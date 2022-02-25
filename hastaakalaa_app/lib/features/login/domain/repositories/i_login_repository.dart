9b3b89f947d7581ee477f1f26232b97cd816cd60
BisheshDangol <bishesh01dangol@gmail.com>
2022-02-25 15:39:15 +0545

    Changing repository name to create user token

--- hastaakalaa_app/lib/features/login/domain/repositories/i_login_repository.dart
+++ hastaakalaa_app/lib/features/login/domain/repositories/i_login_repository.dart
@@ -2,6 +2,6 @@ import 'package:dartz/dartz.dart';
 import 'package:hastaakalaa_app/core/errors/failures.dart';
 
 abstract class ILoginRepository {
-  Future<Either<Failure, Unit>> createPatient(
+  Future<Either<Failure, Unit>> createUserToken(
       {required Map<String, dynamic> data});
 }
