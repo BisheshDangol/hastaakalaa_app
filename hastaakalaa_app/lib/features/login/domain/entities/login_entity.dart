0b0fa5b7f5b13d7075770c9a0b23e4824432ad83
BisheshDangol <bishesh01dangol@gmail.com>
2022-02-25 15:25:33 +0545

    Creating a login entity

---
+++ hastaakalaa_app/lib/features/login/domain/entities/login_entity.dart
@@ -0,0 +1,11 @@
+import 'package:equatable/equatable.dart';
+
+class LoginEntity extends Equatable {
+  final String username;
+  final String password;
+
+  LoginEntity({required this.username, required this.password});
+
+  @override
+  List<Object?> get props => [username, password];
+}
