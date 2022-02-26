99ad8386121eb8585fddbc6bb09ff77b58b5859e
BisheshDangol <bishesh01dangol@gmail.com>
2022-02-26 22:57:26 +0545

    Adding endpoint of create post

--- hastaakalaa_app/lib/core/end_points.dart
+++ hastaakalaa_app/lib/core/end_points.dart
@@ -3,3 +3,5 @@ const baseUrl = 'http://10.0.2.2:8000/';
 const getUserToken = '${baseUrl}token/';
 
 const createUserEndPoint = '${baseUrl}api/user/create/';
+
+const createPostEndPoint = '${baseUrl}api/art/create_art/';
