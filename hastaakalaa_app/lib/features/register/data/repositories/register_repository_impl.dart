f8d94ea8c243a06c8ce01e6fb08e642e3333ee11
BisheshDangol <bishesh01dangol@gmail.com>
2022-02-26 11:27:43 +0545

    Implementing the register repository

---
+++ hastaakalaa_app/lib/features/register/data/repositories/register_repository_impl.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:hastaakalaa_app/core/errors/failures.dart';
+import 'package:dartz/dartz.dart';
+import 'package:hastaakalaa_app/core/network/network_info.dart';
+import 'package:hastaakalaa_app/features/login/data/datasources/login_remote_data_source.dart';
+import 'package:hastaakalaa_app/features/login/domain/repositories/i_login_repository.dart';
+import 'package:hastaakalaa_app/features/register/data/datasources/register_remote_data_source.dart';
+import 'package:hastaakalaa_app/features/register/domain/repositories/i_register_repository.dart';
+
+class RegisterRepositoryImpl implements IRegisterRepository {
+  final IRegisterDataSource remoteDataSource;
+  final NetworkInfo networkInfo;
+
+  RegisterRepositoryImpl({
+    required this.remoteDataSource,
+    required this.networkInfo,
+  });
+
+  @override
+  Future<Either<Failure, Unit>> registerUser(
+      {required Map<String, dynamic> data}) async {
+    if (await networkInfo.isConnected) {
+      try {
+        final remoteList = await remoteDataSource.createUser(data: data);
+        debugPrint('This is the returned number $remoteList');
+        return Right(remoteList);
+      } on ServerFailure {
+        return Left(ServerFailure());
+      }
+    }
+    throw UnimplementedError();
+  }
+}
