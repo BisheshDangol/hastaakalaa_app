40ff09771c64be267b05279cce17f8bec2b6c86d
BisheshDangol <bishesh01dangol@gmail.com>
2022-02-26 10:54:03 +0545

    Creating a call method to the abstract repository

---
+++ hastaakalaa_app/lib/features/register/domain/usecases/register_user_usecase.dart
@@ -0,0 +1,22 @@
+import 'package:equatable/equatable.dart';
+import 'package:hastaakalaa_app/core/errors/failures.dart';
+import 'package:dartz/dartz.dart';
+import 'package:hastaakalaa_app/core/usecase.dart';
+import 'package:hastaakalaa_app/features/register/domain/repositories/i_register_repository.dart';
+
+class RegisterUserUsecase implements Usecase<Unit, Map<String, dynamic>> {
+  IRegisterRepository repository;
+  RegisterUserUsecase(this.repository);
+
+  @override
+  Future<Either<Failure, Unit>> call(Map<String, dynamic> params) async {
+    return await repository.registerUser(data: params);
+  }
+}
+
+class Params extends Equatable {
+  final Map<String, dynamic> data;
+  const Params({required this.data});
+  @override
+  List<Object?> get props => [data];
+}
