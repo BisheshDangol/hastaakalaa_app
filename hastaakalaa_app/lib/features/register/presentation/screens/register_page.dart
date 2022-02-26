e18c1f5b294881e7cb085208321cbd65ff0cb145
BisheshDangol <bishesh01dangol@gmail.com>
2022-02-26 14:31:06 +0545

    Creating username textfield

--- hastaakalaa_app/lib/features/register/presentation/screens/register_page.dart
+++ hastaakalaa_app/lib/features/register/presentation/screens/register_page.dart
@@ -25,6 +25,7 @@ class RegisterPage extends StatelessWidget {
                   child: Column(
                     children: [
                       EmailTextFormField(),
+                      UserNameTextFormField(),
                     ],
                   ),
                 ),
@@ -42,7 +43,7 @@ class EmailTextFormField extends StatelessWidget {
   Widget build(BuildContext context) {
     return TextFormField(
       decoration: InputDecoration(
-        hintText: 'Email',
+        hintText: 'Enter you email address',
         labelText: 'Email',
         errorStyle: TextStyle(fontSize: 13),
         errorText: context
@@ -61,6 +62,36 @@ class EmailTextFormField extends StatelessWidget {
               .read<RegisterFormBloc>()
               .state
               .email
+              .fold((e) => e.msg.toString(), (_) => null)
+          : null,
+    );
+  }
+}
+
+class UserNameTextFormField extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      decoration: InputDecoration(
+        hintText: 'Enter your username',
+        labelText: 'Username',
+        errorStyle: TextStyle(fontSize: 13),
+        errorText: context
+            .read<RegisterFormBloc>()
+            .state
+            .userName
+            .fold((l) => l.msg, (r) => null),
+      ),
+      onChanged: (value) {
+        context
+            .read<RegisterFormBloc>()
+            .add(RegisterFormEvent.changedUserName(userName: value));
+      },
+      validator: (_) => context.read<RegisterFormBloc>().state.showErrors
+          ? context
+              .read<RegisterFormBloc>()
+              .state
+              .userName
               .fold((e) => e.msg.toString(), (_) => null)
           : null,
     );
