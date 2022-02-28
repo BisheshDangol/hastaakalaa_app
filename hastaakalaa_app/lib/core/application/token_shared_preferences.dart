2871bae7eabc9193a79beacd374b5c8b944d4c81
BisheshDangol <bishesh01dangol@gmail.com>
2022-02-28 13:57:33 +0545

    Creaing a method to get the set token

--- hastaakalaa_app/lib/core/application/token_shared_preferences.dart
+++ hastaakalaa_app/lib/core/application/token_shared_preferences.dart
@@ -10,4 +10,9 @@ class TokenSharedPrefernces {
     SharedPreferences myPrefs = await SharedPreferences.getInstance();
     myPrefs.setString(key, value);
   }
+
+  Future<String> getStringValue(String key) async {
+    SharedPreferences myPrefs = await SharedPreferences.getInstance();
+    return myPrefs.getString(key) ?? "";
+  }
 }
