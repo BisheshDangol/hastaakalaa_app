dce10cee4915203db1d4ff6b4ba4128abb70a287
BisheshDangol <bishesh01dangol@gmail.com>
2022-03-01 09:21:44 +0545

    Removing the persisted nav bar when logging out

--- hastaakalaa_app/lib/core/test_screens/dashboard_page.dart
+++ hastaakalaa_app/lib/core/test_screens/dashboard_page.dart
@@ -1,10 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:hastaakalaa_app/core/application/token_shared_preferences.dart';
+import 'package:hastaakalaa_app/features/login/presentation/screens/login_page.dart';
 
 class DashboardPage extends StatelessWidget {
   const DashboardPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Dashboard'),
+        ),
+        drawer: Drawer(
+            child: ListView(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                begin: Alignment.bottomCenter,
+                end: Alignment.topCenter,
+                colors: [
+                  Colors.red,
+                  Colors.white,
+                ],
+              )),
+              height: MediaQuery.of(context).size.height * 0.22,
+              width: MediaQuery.of(context).size.width,
+              padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Text(
+                    'Hastaakalaa',
+                    style: TextStyle(
+                      fontSize: 25.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            ListTile(
+              title: Text('Log out'),
+              onTap: () async {
+                TokenSharedPrefernces.instance.removeToken("token");
+                await Future.delayed(const Duration(milliseconds: 2000), () {
+                  CircularProgressIndicator(color: Colors.red);
+                });
+                Navigator.of(context, rootNavigator: true)
+                    .push(MaterialPageRoute(builder: (context) => LoginPage()));
+              },
+            )
+          ],
+        )));
   }
 }
