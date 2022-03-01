1991af18a5f495a7574c8ed1b8c19856ddb4040e
BisheshDangol <bishesh01dangol@gmail.com>
2022-03-01 19:57:17 +0545

    Creating get art list abstract method

--- hastaakalaa_app/lib/features/art/domain/repositories/i_art_repository.dart
+++ hastaakalaa_app/lib/features/art/domain/repositories/i_art_repository.dart
@@ -1,7 +1,10 @@
 import 'package:dartz/dartz.dart';
 import 'package:hastaakalaa_app/core/errors/failures.dart';
+import 'package:hastaakalaa_app/features/art/domain/entities/art_entity.dart';
 
 abstract class IArtRepository {
   Future<Either<Failure, Unit>> createArtPost(
       {required Map<String, dynamic> data});
+
+  Future<Either<Failure, List<ArtEntity>>> getArtList();
 }
