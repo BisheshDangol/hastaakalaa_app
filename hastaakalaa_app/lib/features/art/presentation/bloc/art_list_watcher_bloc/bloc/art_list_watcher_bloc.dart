66bb4fe590bc76cfcb04011493ef39d011b022f0
BisheshDangol <bishesh01dangol@gmail.com>
2022-03-06 12:28:12 +0545

    Populating bloc art list

--- hastaakalaa_app/lib/features/art/presentation/bloc/art_list_watcher_bloc/bloc/art_list_watcher_bloc.dart
+++ hastaakalaa_app/lib/features/art/presentation/bloc/art_list_watcher_bloc/bloc/art_list_watcher_bloc.dart
@@ -1,7 +1,9 @@
 import 'package:bloc/bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:hastaakalaa_app/core/errors/failures.dart';
+import 'package:hastaakalaa_app/core/usecase.dart';
 import 'package:hastaakalaa_app/features/art/domain/entities/art_entity.dart';
+import 'package:hastaakalaa_app/features/art/domain/usecases/get_all_art_post_list_usecase.dart';
 import 'package:meta/meta.dart';
 
 part 'art_list_watcher_bloc.freezed.dart';
@@ -10,9 +12,22 @@ part 'art_list_watcher_state.dart';
 
 class ArtListWatcherBloc
     extends Bloc<ArtListWatcherEvent, ArtListWatcherState> {
-  ArtListWatcherBloc() : super(ArtListWatcherState.initial()) {
-    on<ArtListWatcherEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+  final GetAllArtPostListUsecase _getAllArtPostListUsecase;
+  ArtListWatcherBloc(this._getAllArtPostListUsecase)
+      : super(ArtListWatcherState.initial()) {
+    on<ArtListWatcherEvent>(
+      (event, emit) async {
+        await event.map(
+          retrieveDoctorList: (_) async {
+            emit(ArtListWatcherState.loading());
+
+            final doctorList = await _getAllArtPostListUsecase.call(NoParams());
+
+            doctorList.fold((l) => emit(ArtListWatcherState.failed(l)),
+                (r) => emit(ArtListWatcherState.loaded(r)));
+          },
+        );
+      },
+    );
   }
 }
