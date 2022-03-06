a45beb1a1fbc020d3c6fec8d0b4022ad1e39e695
BisheshDangol <bishesh01dangol@gmail.com>
2022-03-06 10:56:51 +0545

    Adding freezed annotation in pubspec file

--- hastaakalaa_app/lib/features/art/presentation/bloc/art_list_watcher_bloc/art_list_watcher_event.dart
+++
@@ -1,4 +0,0 @@
-part of 'art_list_watcher_bloc.dart';
-
-@immutable
-abstract class ArtListWatcherEvent {}
--- hastaakalaa_app/lib/features/art/presentation/bloc/art_list_watcher_bloc/art_list_watcher_state.dart
+++
@@ -1,6 +0,0 @@
-part of 'art_list_watcher_bloc.dart';
-
-@immutable
-abstract class ArtListWatcherState {}
-
-class ArtListWatcherInitial extends ArtListWatcherState {}
