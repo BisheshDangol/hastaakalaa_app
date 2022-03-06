cbdfc019bfca2aa4fbe457cb6f7fc173488f5e44
BisheshDangol <bishesh01dangol@gmail.com>
2022-03-06 12:43:46 +0545

    Adding bloc provider in new screen

--- hastaakalaa_app/lib/features/art/presentation/screens/dashboard_page.dart
+++ hastaakalaa_app/lib/features/art/presentation/screens/dashboard_page.dart
@@ -1,60 +1,68 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hastaakalaa_app/core/application/token_shared_preferences.dart';
+import 'package:hastaakalaa_app/features/art/presentation/bloc/art_list_watcher_bloc/bloc/art_list_watcher_bloc.dart';
 import 'package:hastaakalaa_app/features/login/presentation/screens/login_page.dart';
+
+import '../../../../injection_container.dart';
 
 class DashboardPage extends StatelessWidget {
   const DashboardPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Dashboard'),
-      ),
-      drawer: Drawer(
-        child: ListView(
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                begin: Alignment.bottomCenter,
-                end: Alignment.topCenter,
-                colors: [
-                  Colors.red,
-                  Colors.white,
-                ],
-              )),
-              height: MediaQuery.of(context).size.height * 0.22,
-              width: MediaQuery.of(context).size.width,
-              padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  Text(
-                    'Hastaakalaa',
-                    style: TextStyle(
-                      fontSize: 25.0,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  )
-                ],
+    return BlocProvider(
+      create: (context) => sl<ArtListWatcherBloc>()
+        ..add(ArtListWatcherEvent.retrieveDoctorList()),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Dashboard'),
+        ),
+        drawer: Drawer(
+          child: ListView(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                  begin: Alignment.bottomCenter,
+                  end: Alignment.topCenter,
+                  colors: [
+                    Colors.red,
+                    Colors.white,
+                  ],
+                )),
+                height: MediaQuery.of(context).size.height * 0.22,
+                width: MediaQuery.of(context).size.width,
+                padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Text(
+                      'Hastaakalaa',
+                      style: TextStyle(
+                        fontSize: 25.0,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    )
+                  ],
+                ),
               ),
-            ),
-            ListTile(
-              title: Text('Log out'),
-              onTap: () async {
-                await Future.delayed(
-                  const Duration(milliseconds: 500),
-                );
-                TokenSharedPrefernces.instance.removeToken("token");
+              ListTile(
+                title: Text('Log out'),
+                onTap: () async {
+                  await Future.delayed(
+                    const Duration(milliseconds: 500),
+                  );
+                  TokenSharedPrefernces.instance.removeToken("token");
 
-                Navigator.of(context, rootNavigator: true)
-                    .push(MaterialPageRoute(builder: (context) => LoginPage()));
-              },
-            )
-          ],
+                  Navigator.of(context, rootNavigator: true).push(
+                      MaterialPageRoute(builder: (context) => LoginPage()));
+                },
+              )
+            ],
+          ),
         ),
       ),
     );
