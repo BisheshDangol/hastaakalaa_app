bebe1ee4bdf81784284ec5d99b3e83a4d6626878
BisheshDangol <bishesh01dangol@gmail.com>
2022-03-29 21:36:55 +0545

    Making the user entity equatable

---
+++ hastaakalaa_app/lib/features/user/domain/entities/user_entity.dart
@@ -0,0 +1,38 @@
+import 'package:equatable/equatable.dart';
+
+class UserEntity extends Equatable {
+  String email;
+  String userName;
+  String firstName;
+  String lastName;
+  String phoneNumber;
+  String address;
+  String userType;
+  List follower;
+  List followedBy;
+
+  UserEntity({
+    required this.address,
+    required this.email,
+    required this.firstName,
+    required this.followedBy,
+    required this.follower,
+    required this.lastName,
+    required this.phoneNumber,
+    required this.userName,
+    required this.userType,
+  });
+
+  @override
+  List<Object?> get props => [
+        address,
+        email,
+        firstName,
+        followedBy,
+        follower,
+        lastName,
+        phoneNumber,
+        userName,
+        userType
+      ];
+}
