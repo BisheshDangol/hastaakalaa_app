6a09313130be6a52f4736c1a4cedeaab91af3adc
BisheshDangol <bishesh01dangol@gmail.com>
2022-03-29 20:50:32 +0545

    Creating art details page

--- hastaakalaa_app/lib/features/art/presentation/screens/art_detail_page.dart
+++ hastaakalaa_app/lib/features/art/presentation/screens/art_detail_page.dart
@@ -1,1 +1,10 @@
+import 'package:flutter/material.dart';
 
+class ArtDetailPage extends StatelessWidget {
+  const ArtDetailPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
