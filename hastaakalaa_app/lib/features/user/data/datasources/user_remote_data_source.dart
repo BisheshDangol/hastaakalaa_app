cff6164e64c9339208492413687b51033223c5fd
BisheshDangol <bishesh01dangol@gmail.com>
2022-03-30 11:01:30 +0545

    Getting data from the remote source

---
+++ hastaakalaa_app/lib/features/user/data/datasources/user_remote_data_source.dart
@@ -0,0 +1,29 @@
+import 'dart:convert';
+import 'package:hastaakalaa_app/core/end_points.dart';
+import 'package:hastaakalaa_app/core/errors/exceptions.dart';
+import 'package:hastaakalaa_app/features/user/data/models/user_model.dart';
+import 'package:http/http.dart' as http;
+
+abstract class IUserDataSource {
+  Future<List<UserModel>> getAllUser();
+}
+
+class UserRemoteDataSource implements IUserDataSource {
+  final http.Client client;
+  UserRemoteDataSource({required this.client});
+  @override
+  Future<List<UserModel>> getAllUser() async {
+    final response = await client.get(
+      Uri.parse(retrieveArtList),
+    );
+
+    if (response.statusCode == 200) {
+      final jsonData = jsonDecode(response.body) as List;
+      return jsonData
+          .map((e) => UserModel.fromJson(e as Map<String, dynamic>))
+          .toList();
+    } else {
+      throw ServerException();
+    }
+  }
+}
