523f6f3ed195c53f85c9d0fc2d072e5d7b61c9f7
BisheshDangol <bishesh01dangol@gmail.com>
2022-03-30 11:10:54 +0545

    Implementing repository of user

--- hastaakalaa_app/lib/features/user/data/repositories/user_repository_impl.dart
+++ hastaakalaa_app/lib/features/user/data/repositories/user_repository_impl.dart
@@ -1,16 +1,34 @@
-// import 'package:flutter/material.dart';
-// import 'package:hastaakalaa_app/core/network/network_info.dart';
-// import 'package:hastaakalaa_app/features/user/domain/entities/user_entity.dart';
-// import 'package:hastaakalaa_app/core/errors/failures.dart';
-// import 'package:dartz/dartz.dart';
-// import 'package:hastaakalaa_app/features/user/domain/repositories/i_user_repository.dart';
+import 'package:flutter/material.dart';
+import 'package:hastaakalaa_app/core/network/network_info.dart';
+import 'package:hastaakalaa_app/features/user/data/datasources/user_remote_data_source.dart';
+import 'package:hastaakalaa_app/features/user/domain/entities/user_entity.dart';
+import 'package:hastaakalaa_app/core/errors/failures.dart';
+import 'package:dartz/dartz.dart';
+import 'package:hastaakalaa_app/features/user/domain/repositories/i_user_repository.dart';
 
-// class UserRepositoryImpl implements IUserRepository {
+class UserRepositoryImpl implements IUserRepository {
+  final IUserDataSource remoteDataSource;
+  final NetworkInfo networkInfo;
+  UserRepositoryImpl({
+    required this.remoteDataSource,
+    required this.networkInfo,
+  });
+  @override
+  Future<Either<Failure, List<UserEntity>>> getAllUser() async {
+    if (await networkInfo.isConnected) {
+      try {
+        final remoteList = await remoteDataSource.getAllUser();
+        debugPrint('This is the returned list: $remoteList');
+        return Right(remoteList);
+      } on ServerFailure {
+        return Left(ServerFailure());
+      }
+    }
+    throw UnimplementedError();
+  }
+}
 
 
-//   // UserRepositoryImpl({
-//   //   required this.remoteDataSource,
-//   //   required this.networkInfo,
-//   // });
+  
 
 // }
