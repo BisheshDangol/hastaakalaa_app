751d07d2e2735b9e9d2f78e078467ccc895cbedc
BisheshDangol <bishesh01dangol@gmail.com>
2022-03-30 10:04:16 +0545

    Creating a get user usecase

---
+++ hastaakalaa_app/lib/features/user/domain/usecases/get_all_user_usecase.dart
@@ -0,0 +1,14 @@
+import 'package:hastaakalaa_app/core/errors/failures.dart';
+import 'package:dartz/dartz.dart';
+import 'package:hastaakalaa_app/core/usecase.dart';
+import 'package:hastaakalaa_app/features/user/domain/entities/user_entity.dart';
+import 'package:hastaakalaa_app/features/user/domain/repositories/i_user_repository.dart';
+
+class GetAllUserUsecase implements Usecase<List<UserEntity>, NoParams> {
+  final IUserRepository repository;
+  GetAllUserUsecase(this.repository);
+  @override
+  Future<Either<Failure, List<UserEntity>>> call(NoParams params) async {
+    return await repository.getAllUser();
+  }
+}
