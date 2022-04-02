c40cb4344e5473094a8187156155b5aaca9a9e41
BisheshDangol <bishesh01dangol@gmail.com>
2022-04-02 09:17:47 +0545

    Calling bookmark post usecase

---
+++ hastaakalaa_app/lib/features/art/domain/usecases/bookmark_post_usecase.dart
@@ -0,0 +1,24 @@
+import 'package:equatable/equatable.dart';
+import 'package:dartz/dartz.dart';
+import 'package:hastaakalaa_app/core/errors/failures.dart';
+import 'package:hastaakalaa_app/core/usecase.dart';
+import 'package:hastaakalaa_app/features/art/domain/repositories/i_art_repository.dart';
+
+class BookmarkPostUsecase implements Usecase<String, int?> {
+  IArtRepository repository;
+
+  BookmarkPostUsecase(this.repository);
+  @override
+  Future<Either<Failure, String>> call(int? params) async {
+    return await repository.likePost(data: params);
+  }
+}
+
+class Params extends Equatable {
+  final int data;
+
+  Params({required this.data});
+
+  @override
+  List<Object> get props => [data];
+}
