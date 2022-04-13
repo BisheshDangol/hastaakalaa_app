7e8b7fbe173aaa4ab1f5d84bd408b1b1e91463e7
BisheshDangol <bishesh01dangol@gmail.com>
2022-04-13 15:41:38 +0545

    Implementing sell art watcher bloc

--- hastaakalaa_app/lib/features/art/presentation/bloc/sell_art_watcher_bloc/sell_art_watcher_bloc.dart
+++ hastaakalaa_app/lib/features/art/presentation/bloc/sell_art_watcher_bloc/sell_art_watcher_bloc.dart
@@ -1,7 +1,9 @@
 import 'package:bloc/bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:hastaakalaa_app/core/errors/failures.dart';
+import 'package:hastaakalaa_app/core/usecase.dart';
 import 'package:hastaakalaa_app/features/art/domain/entities/art_entity.dart';
+import 'package:hastaakalaa_app/features/art/domain/usecases/sell_art_post_usecase.dart';
 import 'package:meta/meta.dart';
 
 part 'sell_art_watcher_event.dart';
@@ -10,9 +12,18 @@ part 'sell_art_watcher_bloc.freezed.dart';
 
 class SellArtWatcherBloc
     extends Bloc<SellArtWatcherEvent, SellArtWatcherState> {
-  SellArtWatcherBloc() : super(SellArtWatcherState.initial()) {
-    on<SellArtWatcherEvent>((event, emit) {
-      // TODO: implement event handler
+  final SellArtPostUsecase _sellArtPostUsecase;
+  SellArtWatcherBloc(this._sellArtPostUsecase)
+      : super(SellArtWatcherState.initial()) {
+    on<SellArtWatcherEvent>((event, emit) async {
+      await event.map(retrieveArtList: (_) async {
+        emit(SellArtWatcherState.loading());
+
+        final artList = await _sellArtPostUsecase.call(NoParams());
+
+        artList.fold((l) => emit(SellArtWatcherState.failed(l)),
+            (r) => emit(SellArtWatcherState.loaded(r)));
+      });
     });
   }
 }
