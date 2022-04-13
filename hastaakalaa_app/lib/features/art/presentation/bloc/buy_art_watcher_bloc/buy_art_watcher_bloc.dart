eb4af90b7b7cbd44b8458c71a1eb6fba59b7be80
BisheshDangol <bishesh01dangol@gmail.com>
2022-04-13 12:52:33 +0545

    Creating states for buy art bloc

--- hastaakalaa_app/lib/features/art/presentation/bloc/buy_art_watcher_bloc/buy_art_watcher_bloc.dart
+++ hastaakalaa_app/lib/features/art/presentation/bloc/buy_art_watcher_bloc/buy_art_watcher_bloc.dart
@@ -1,11 +1,15 @@
 import 'package:bloc/bloc.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:hastaakalaa_app/core/errors/failures.dart';
+import 'package:hastaakalaa_app/features/art/domain/entities/art_entity.dart';
 import 'package:meta/meta.dart';
 
 part 'buy_art_watcher_event.dart';
 part 'buy_art_watcher_state.dart';
+part 'buy_art_watcher_bloc.freezed.dart';
 
 class BuyArtWatcherBloc extends Bloc<BuyArtWatcherEvent, BuyArtWatcherState> {
-  BuyArtWatcherBloc() : super(BuyArtWatcherInitial()) {
+  BuyArtWatcherBloc() : super(BuyArtWatcherState.initial()) {
     on<BuyArtWatcherEvent>((event, emit) {
       // TODO: implement event handler
     });
--- hastaakalaa_app/lib/features/art/presentation/bloc/buy_art_watcher_bloc/buy_art_watcher_state.dart
+++ hastaakalaa_app/lib/features/art/presentation/bloc/buy_art_watcher_bloc/buy_art_watcher_state.dart
@@ -1,6 +1,9 @@
 part of 'buy_art_watcher_bloc.dart';
 
-@immutable
-abstract class BuyArtWatcherState {}
-
-class BuyArtWatcherInitial extends BuyArtWatcherState {}
+@freezed
+class BuyArtWatcherState with _$BuyArtWatcherState {
+  const factory BuyArtWatcherState.initial() = _Initial;
+  const factory BuyArtWatcherState.loading() = _Loading;
+  const factory BuyArtWatcherState.loaded(List<ArtEntity> artList) = _Loaded;
+  const factory BuyArtWatcherState.failed(Failure failure) = _Failed;
+}
