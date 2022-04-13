23cd9e32650503d1c33fb8b7d6896a732db4723e
BisheshDangol <bishesh01dangol@gmail.com>
2022-04-13 12:38:59 +0545

    Implementing buy art post usecase

---
+++ hastaakalaa_app/lib/features/art/domain/usecases/buy_art_post_usecase.dart
@@ -0,0 +1,15 @@
+import 'package:dartz/dartz.dart';
+import 'package:hastaakalaa_app/core/errors/failures.dart';
+import 'package:hastaakalaa_app/core/usecase.dart';
+import 'package:hastaakalaa_app/features/art/domain/entities/art_entity.dart';
+import 'package:hastaakalaa_app/features/art/domain/repositories/i_art_repository.dart';
+
+class BuyArtPostUsecase implements Usecase<List<ArtEntity>, NoParams> {
+  final IArtRepository repository;
+  BuyArtPostUsecase(this.repository);
+
+  @override
+  Future<Either<Failure, List<ArtEntity>>> call(NoParams params) async {
+    return await repository.getAllArtPost();
+  }
+}
