c58922e4367280dd7ca333cd0e594db41a13d1ce
BisheshDangol <bishesh01dangol@gmail.com>
2022-04-13 12:57:25 +0545

    Implementing get art list in bloc

--- hastaakalaa_app/lib/features/art/presentation/bloc/buy_art_watcher_bloc/buy_art_watcher_bloc.dart
+++ hastaakalaa_app/lib/features/art/presentation/bloc/buy_art_watcher_bloc/buy_art_watcher_bloc.dart
@@ -1,7 +1,9 @@
 import 'package:bloc/bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:hastaakalaa_app/core/errors/failures.dart';
+import 'package:hastaakalaa_app/core/usecase.dart';
 import 'package:hastaakalaa_app/features/art/domain/entities/art_entity.dart';
+import 'package:hastaakalaa_app/features/art/domain/usecases/buy_art_post_usecase.dart';
 import 'package:meta/meta.dart';
 
 part 'buy_art_watcher_event.dart';
@@ -9,9 +11,18 @@ part 'buy_art_watcher_state.dart';
 part 'buy_art_watcher_bloc.freezed.dart';
 
 class BuyArtWatcherBloc extends Bloc<BuyArtWatcherEvent, BuyArtWatcherState> {
-  BuyArtWatcherBloc() : super(BuyArtWatcherState.initial()) {
-    on<BuyArtWatcherEvent>((event, emit) {
-      // TODO: implement event handler
+  final BuyArtPostUsecase _buyArtPostUsecase;
+  BuyArtWatcherBloc(this._buyArtPostUsecase)
+      : super(BuyArtWatcherState.initial()) {
+    on<BuyArtWatcherEvent>((event, emit) async {
+      await event.map(retrieveArtList: (_) async {
+        emit(BuyArtWatcherState.loading());
+
+        final artList = await _buyArtPostUsecase.call(NoParams());
+
+        artList.fold((l) => emit(BuyArtWatcherState.failed(l)),
+            (r) => emit(BuyArtWatcherState.loaded(r)));
+      });
     });
   }
 }
