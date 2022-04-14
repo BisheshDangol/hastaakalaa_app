146f053b34ef677aa8ed96896a3f9438a48384de
BisheshDangol <bishesh01dangol@gmail.com>
2022-04-14 08:14:03 +0545

    Implementing search user in usecase

--- hastaakalaa_app/lib/features/user/domain/usecases/search_user_usecase.dart
+++ hastaakalaa_app/lib/features/user/domain/usecases/search_user_usecase.dart
@@ -1,1 +1,27 @@
+import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
+import 'package:hastaakalaa_app/core/errors/failures.dart';
+import 'package:hastaakalaa_app/core/usecase.dart';
+import 'package:hastaakalaa_app/features/art/domain/entities/art_entity.dart';
+import 'package:hastaakalaa_app/features/art/domain/repositories/i_art_repository.dart';
+import 'package:hastaakalaa_app/features/user/domain/entities/user_entity.dart';
+import 'package:hastaakalaa_app/features/user/domain/repositories/i_user_repository.dart';
 
+class SearchUserUseCase implements Usecase<List<UserEntity>, String?> {
+  final IUserRepository repository;
+  SearchUserUseCase(this.repository);
+
+  @override
+  Future<Either<Failure, List<UserEntity>>> call(String? params) async {
+    return await repository.searchUser(data: params);
+  }
+}
+
+class Params extends Equatable {
+  final String data;
+
+  Params({required this.data});
+
+  @override
+  List<Object> get props => [data];
+}
