935b6cb0ab34113c4d443b5a5d55c0e5dd934669
BisheshDangol <bishesh01dangol@gmail.com>
2022-04-14 09:15:51 +0545

    Creating search user wrapper

---
+++ hastaakalaa_app/lib/features/user/presentation/screens/search_user_page.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hastaakalaa_app/core/wrapper/search_wrapper.dart';
+import 'package:hastaakalaa_app/features/art/domain/entities/art_entity.dart';
+import 'package:hastaakalaa_app/features/art/presentation/bloc/art_search_watcher_bloc/art_search_watcher_bloc.dart';
+import 'package:hastaakalaa_app/features/art/presentation/screens/art_filter_page.dart';
+import 'package:hastaakalaa_app/features/user/domain/entities/user_entity.dart';
+import 'package:hastaakalaa_app/features/user/presentation/bloc/search_user_watcher_bloc/search_user_watcher_bloc.dart';
+import 'package:hastaakalaa_app/injection_container.dart';
+
+class SearchUserPage extends StatelessWidget {
+  const SearchUserPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (_) => sl<SearchUserWatcherBloc>(),
+      child: Scaffold(
+        body: SafeArea(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  child: Row(
+                    children: [
+                      Flexible(child: SearchUserTextFormField(), flex: 3),
+                      SearchButton(),
+                    ],
+                  ),
+                ),
+              ),
+              PageBuilder(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class PageBuilder extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<SearchUserWatcherBloc, SearchUserWatcherState>(
+      builder: (context, state) {
+        return Expanded(
+          child: ListView(
+            children: [
+              state.failureOrSuccess == null
+                  ? Container()
+                  : state.failureOrSuccess!.fold(
+                      (l) => CircularProgressIndicator(),
+                      (r) => UserContainer(r))
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
+
+class UserContainer extends StatelessWidget {
+  final List<UserEntity> userList;
+  UserContainer(this.userList);
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      physics: const AlwaysScrollableScrollPhysics(),
+      scrollDirection: Axis.vertical,
+      shrinkWrap: true,
+      children: userList
+          .map((e) => SearchWrapper(
+                artEntity: e,
+              ))
+          .toList(),
+    );
+  }
+}
+
+class SearchUserTextFormField extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      decoration: InputDecoration(
+        focusedBorder: OutlineInputBorder(
+          borderSide: const BorderSide(color: Colors.blue, width: 2.0),
+          borderRadius: BorderRadius.circular(5.0),
+        ),
+        labelText: 'Search',
+        errorStyle: TextStyle(fontSize: 13),
+      ),
+      onChanged: (value) {
+        context
+            .read<ArtSearchWatcherBloc>()
+            .add(ArtSearchWatcherEvent.changedKeyword(keyword: value));
+      },
+      validator: (_) => context.read<ArtSearchWatcherBloc>().state.showErrors
+          ? context
+              .read<ArtSearchWatcherBloc>()
+              .state
+              .keywordTitle
+              .fold((e) => e.msg.toString(), (_) => null)
+          : null,
+    );
+  }
+}
+
+class SearchButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      onPressed: () {
+        context
+            .read<ArtSearchWatcherBloc>()
+            .add(ArtSearchWatcherEvent.pressedSearch());
+      },
+      icon: Icon(Icons.search),
+    );
+  }
+}
