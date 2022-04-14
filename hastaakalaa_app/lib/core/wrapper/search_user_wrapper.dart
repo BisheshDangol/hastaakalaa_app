3970c2a627f380ca134c21794786e437ff1177ce
BisheshDangol <bishesh01dangol@gmail.com>
2022-04-14 10:46:44 +0545

    Inserting ink well in search container

--- hastaakalaa_app/lib/core/wrapper/search_user_wrapper.dart
+++ hastaakalaa_app/lib/core/wrapper/search_user_wrapper.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hastaakalaa_app/features/comment/domain/entities/comment_entity.dart';
 import 'package:hastaakalaa_app/features/user/domain/entities/user_entity.dart';
+import 'package:hastaakalaa_app/features/user/presentation/screens/user_page.dart';
 
 class SearchUserWrapper extends StatelessWidget {
   final UserEntity userEntity;
@@ -9,30 +10,34 @@ class SearchUserWrapper extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      // height: MediaQuery.of(context).size.height / 10,
-      width: MediaQuery.of(context).size.width,
-      child: Card(
-        elevation: 3.0,
-        child: Column(
-          children: [
-            Row(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: CircleAvatar(
-                    backgroundColor: Colors.grey,
-                    radius: 25,
+    return InkWell(
+      onTap: () => Navigator.push(
+          context, MaterialPageRoute(builder: (context) => UserPage())),
+      child: Container(
+        // height: MediaQuery.of(context).size.height / 10,
+        width: MediaQuery.of(context).size.width,
+        child: Card(
+          elevation: 3.0,
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: CircleAvatar(
+                      backgroundColor: Colors.grey,
+                      radius: 25,
+                    ),
                   ),
-                ),
-                SizedBox(
-                  width: 5,
-                ),
-                Text(userEntity.firstName.toString(),
-                    style: TextStyle(fontSize: 20)),
-              ],
-            ),
-          ],
+                  SizedBox(
+                    width: 5,
+                  ),
+                  Text(userEntity.firstName.toString(),
+                      style: TextStyle(fontSize: 20)),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
