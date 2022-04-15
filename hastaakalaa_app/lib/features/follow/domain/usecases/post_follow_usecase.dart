bcc2d2fbd7c6e0997768ab141372258ab6518e79
BisheshDangol <bishesh01dangol@gmail.com>
2022-04-15 23:19:06 +0545

    Calling post follow repository

--- hastaakalaa_app/lib/features/follow/domain/usecases/post_follow_usecase.dart
+++ hastaakalaa_app/lib/features/follow/domain/usecases/post_follow_usecase.dart
@@ -1,1 +1,22 @@
+import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
+import 'package:hastaakalaa_app/core/errors/failures.dart';
+import 'package:hastaakalaa_app/core/usecase.dart';
+import 'package:hastaakalaa_app/features/follow/domain/repositories/i_follow_repository.dart';
 
+class PostFollowUsecase implements Usecase<String, int?> {
+  IFollowRepository repository;
+
+  PostFollowUsecase(this.repository);
+  @override
+  Future<Either<Failure, String>> call(int? params) async {}
+}
+
+class Params extends Equatable {
+  final int data;
+
+  Params({required this.data});
+
+  @override
+  List<Object> get props => [data];
+}
