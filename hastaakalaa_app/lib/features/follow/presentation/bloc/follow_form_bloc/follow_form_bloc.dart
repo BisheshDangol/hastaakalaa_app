4ef95293b8981044d61cfbaa9f1cccbb6104b6c2
BisheshDangol <bishesh01dangol@gmail.com>
2022-04-15 23:40:36 +0545

    Implementing pressed follow functionality

--- hastaakalaa_app/lib/features/follow/presentation/bloc/follow_form_bloc/follow_form_bloc.dart
+++ hastaakalaa_app/lib/features/follow/presentation/bloc/follow_form_bloc/follow_form_bloc.dart
@@ -3,6 +3,7 @@ import 'package:dartz/dartz.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:hastaakalaa_app/core/errors/failures.dart';
 import 'package:hastaakalaa_app/features/follow/domain/entities/follow_entity.dart';
+import 'package:hastaakalaa_app/features/follow/domain/usecases/post_follow_usecase.dart';
 import 'package:meta/meta.dart';
 
 part 'follow_form_event.dart';
@@ -10,17 +11,30 @@ part 'follow_form_state.dart';
 part 'follow_form_bloc.freezed.dart';
 
 class FollowFormBloc extends Bloc<FollowFormEvent, FollowFormState> {
-  FollowFormBloc() : super(FollowFormState.initial()) {
+  final PostFollowUsecase _postFollowUsecase;
+  FollowFormBloc(this._postFollowUsecase) : super(FollowFormState.initial()) {
     on<FollowFormEvent>((event, emit) async {
-      await event.map(
-          changedId: (_ChangedId value) {
-            emit(
-              state.copyWith(
-                id: value.id,
-              ),
-            );
-          },
-          pressedFollow: (_PressedFollow value) {});
+      await event.map(changedId: (_ChangedId value) {
+        emit(
+          state.copyWith(
+            id: value.id,
+          ),
+        );
+      }, pressedFollow: (_) async {
+        emit(state.copyWith(isLoading: true, failureOrSuccess: null));
+
+        Either<Failure, String>? failureOrSuccess;
+
+        failureOrSuccess = await _postFollowUsecase.call(state.id);
+
+        emit(
+          state.copyWith(
+            isLoading: false,
+            failureOrSuccess: failureOrSuccess,
+            showErrors: true,
+          ),
+        );
+      });
     });
   }
 }
