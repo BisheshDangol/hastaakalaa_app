5882dd6a3c78624584cd5176ff31af0866c99d13
BisheshDangol <bishesh01dangol@gmail.com>
2022-04-15 19:45:59 +0545

    Implementing follow list

---
+++ hastaakalaa_app/lib/features/follow/data/repositories/follow_repository_impl.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:hastaakalaa_app/core/network/network_info.dart';
+import 'package:hastaakalaa_app/features/follow/data/datasources/follow_remote_data_source.dart';
+import 'package:hastaakalaa_app/features/follow/domain/entities/follow_entity.dart';
+import 'package:hastaakalaa_app/core/errors/failures.dart';
+import 'package:dartz/dartz.dart';
+import 'package:hastaakalaa_app/features/follow/domain/repositories/i_follow_repository.dart';
+
+class FollowRepositoryImpl implements IFollowRepository {
+  final IFollowDataSource remoteDataSource;
+  final NetworkInfo networkInfo;
+
+  FollowRepositoryImpl({
+    required this.remoteDataSource,
+    required this.networkInfo,
+  });
+  @override
+  Future<Either<Failure, List<FollowEntity>>> getFollowList() async {
+    if (await networkInfo.isConnected) {
+      try {
+        final remoteList = await remoteDataSource.getFollowList();
+        debugPrint('This is the returned list: $remoteList');
+        return Right(remoteList);
+      } on ServerFailure {
+        return Left(ServerFailure());
+      }
+    }
+    throw UnimplementedError();
+  }
+}
