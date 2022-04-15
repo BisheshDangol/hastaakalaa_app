5bf4a93084b4659152b81a7882c09c30d2f3dc70
BisheshDangol <bishesh01dangol@gmail.com>
2022-04-15 22:53:43 +0545

    Developing bloc to map with events

--- hastaakalaa_app/lib/features/follow/presentation/bloc/get_following_watcher_bloc/get_following_watcher_bloc.dart
+++ hastaakalaa_app/lib/features/follow/presentation/bloc/get_following_watcher_bloc/get_following_watcher_bloc.dart
@@ -2,7 +2,9 @@ import 'package:bloc/bloc.dart';
 import 'package:dartz/dartz.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:hastaakalaa_app/core/errors/failures.dart';
+import 'package:hastaakalaa_app/core/usecase.dart';
 import 'package:hastaakalaa_app/features/follow/domain/entities/follow_entity.dart';
+import 'package:hastaakalaa_app/features/follow/domain/usecases/get_all_following_list_usecase.dart';
 import 'package:meta/meta.dart';
 
 part 'get_following_watcher_event.dart';
@@ -11,9 +13,26 @@ part 'get_following_watcher_bloc.freezed.dart';
 
 class GetFollowingWatcherBloc
     extends Bloc<GetFollowingWatcherEvent, GetFollowingWatcherState> {
-  GetFollowingWatcherBloc() : super(GetFollowingWatcherState.initial()) {
-    on<GetFollowingWatcherEvent>((event, emit) {
-      // TODO: implement event handler
+  final GetAllFollowingListUseCase _allFollowingListUseCase;
+  GetFollowingWatcherBloc(this._allFollowingListUseCase)
+      : super(GetFollowingWatcherState.initial()) {
+    on<GetFollowingWatcherEvent>((event, emit) async {
+      await event.map(retrieveFollowingList: (_) async {
+        emit(state.copyWith(isLoading: true, failureOrSuccess: null));
+
+        final followAllList = await _allFollowingListUseCase.call(NoParams());
+
+        emit(state.copyWith(
+            failureOrSuccess: followAllList,
+            isLoading: false,
+            followingList: followAllList.fold((l) => [], (r) => r)));
+      }, changedId: (_ChangedId value) {
+        emit(
+          state.copyWith(
+            id: value.id,
+          ),
+        );
+      });
     });
   }
 }
