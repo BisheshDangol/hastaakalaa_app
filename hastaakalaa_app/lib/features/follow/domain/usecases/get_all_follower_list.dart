ab718aceb4e8697e25c49b190e8c4c5f0ff0347f
BisheshDangol <bishesh01dangol@gmail.com>
2022-04-15 19:28:21 +0545

    Calling get follow list method with no params

---
+++ hastaakalaa_app/lib/features/follow/domain/usecases/get_all_follower_list.dart
@@ -0,0 +1,15 @@
+import 'package:dartz/dartz.dart';
+import 'package:hastaakalaa_app/core/errors/failures.dart';
+import 'package:hastaakalaa_app/core/usecase.dart';
+import 'package:hastaakalaa_app/features/follow/domain/entities/follow_entity.dart';
+import 'package:hastaakalaa_app/features/follow/domain/repositories/i_follow_repository.dart';
+
+class GetAllFollowerList implements Usecase<List<FollowEntity>, NoParams> {
+  final IFollowRepository repository;
+  GetAllFollowerList(this.repository);
+
+  @override
+  Future<Either<Failure, List<FollowEntity>>> call(NoParams params) async {
+    return await repository.getFollowList();
+  }
+}
