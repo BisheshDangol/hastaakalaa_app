04ecb75a62d0e7ccc4f06e468cdbb7002976b4d7
BisheshDangol <bishesh01dangol@gmail.com>
2022-04-15 19:39:45 +0545

    Getting data from the api through remote data source

--- hastaakalaa_app/lib/features/follow/data/datasources/follow_remote_data_source.dart
+++ hastaakalaa_app/lib/features/follow/data/datasources/follow_remote_data_source.dart
@@ -1,12 +1,36 @@
+import 'dart:convert';
+
+import 'package:hastaakalaa_app/core/application/token_shared_preferences.dart';
+import 'package:hastaakalaa_app/core/end_points.dart';
+import 'package:hastaakalaa_app/core/errors/exceptions.dart';
 import 'package:hastaakalaa_app/features/follow/data/models/follow_model.dart';
+import 'package:http/http.dart' as http;
 
 abstract class IFollowDataSource {
   Future<List<FollowModel>> getFollowList();
 }
 
 class FollowRemoteDataSource implements IFollowDataSource {
+  final http.Client client;
+  FollowRemoteDataSource({required this.client});
   @override
-  Future<List<FollowModel>> getFollowList() {
-    throw UnimplementedError();
+  Future<List<FollowModel>> getFollowList() async {
+    String userToken =
+        await TokenSharedPrefernces.instance.getTokenValue("token");
+    Map<String, String> headers = {
+      "content-type": "application/json",
+      "Authorization": "Token ${userToken}",
+    };
+    final response =
+        await client.get(Uri.parse(retrieveArtPostEndpoint), headers: headers);
+
+    if (response.statusCode == 200) {
+      final jsonData = jsonDecode(response.body) as List;
+      return jsonData
+          .map((e) => FollowModel.fromJson(e as Map<String, dynamic>))
+          .toList();
+    } else {
+      throw ServerException();
+    }
   }
 }
