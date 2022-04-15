d4c95c1d868b901fd0b7ffa44c662a72a6888989
BisheshDangol <bishesh01dangol@gmail.com>
2022-04-15 19:52:30 +0545

    Appending states to the get follow watcher bloc

--- hastaakalaa_app/lib/features/follow/presentation/bloc/get_follow_watcher_bloc/get_follow_watcher_bloc.dart
+++ hastaakalaa_app/lib/features/follow/presentation/bloc/get_follow_watcher_bloc/get_follow_watcher_bloc.dart
@@ -1,11 +1,16 @@
 import 'package:bloc/bloc.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:hastaakalaa_app/core/errors/failures.dart';
+import 'package:hastaakalaa_app/features/follow/domain/entities/follow_entity.dart';
 import 'package:meta/meta.dart';
 
 part 'get_follow_watcher_event.dart';
 part 'get_follow_watcher_state.dart';
+part 'get_follow_watcher_bloc.freezed.dart';
 
-class GetFollowWatcherBloc extends Bloc<GetFollowWatcherEvent, GetFollowWatcherState> {
-  GetFollowWatcherBloc() : super(GetFollowWatcherInitial()) {
+class GetFollowWatcherBloc
+    extends Bloc<GetFollowWatcherEvent, GetFollowWatcherState> {
+  GetFollowWatcherBloc() : super((GetFollowWatcherState.initial())) {
     on<GetFollowWatcherEvent>((event, emit) {
       // TODO: implement event handler
     });
--- hastaakalaa_app/lib/features/follow/presentation/bloc/get_follow_watcher_bloc/get_follow_watcher_state.dart
+++ hastaakalaa_app/lib/features/follow/presentation/bloc/get_follow_watcher_bloc/get_follow_watcher_state.dart
@@ -1,6 +1,10 @@
 part of 'get_follow_watcher_bloc.dart';
 
-@immutable
-abstract class GetFollowWatcherState {}
-
-class GetFollowWatcherInitial extends GetFollowWatcherState {}
+@freezed
+class GetFollowWatcherState with _$GetFollowWatcherState {
+  const factory GetFollowWatcherState.initial() = _Initial;
+  const factory GetFollowWatcherState.loading() = _Loading;
+  const factory GetFollowWatcherState.loaded(List<FollowEntity> artList) =
+      _Loaded;
+  const factory GetFollowWatcherState.failed(Failure failure) = _Failed;
+}
