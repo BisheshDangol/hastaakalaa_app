939a1f8109d538a8a3587797ce1a51f8bf663adc
BisheshDangol <bishesh01dangol@gmail.com>
2022-04-16 19:59:07 +0545

    Adding get following watcher bloc

---
+++ hastaakalaa_app/lib/features/follow/presentation/bloc/get_other_following_watcher_bloc/get_other_following_watcher_bloc.freezed.dart
@@ -0,0 +1,254 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target
+
+part of 'get_other_following_watcher_bloc.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more informations: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+class _$GetOtherFollowingWatcherStateTearOff {
+  const _$GetOtherFollowingWatcherStateTearOff();
+
+  _GetOtherFollowingWatcherState call(
+      {required bool isLoading,
+      int? id,
+      required bool showErrors,
+      List<FollowEntity>? followList,
+      Either<Failure, List<FollowEntity>>? failureOrSuccess}) {
+    return _GetOtherFollowingWatcherState(
+      isLoading: isLoading,
+      id: id,
+      showErrors: showErrors,
+      followList: followList,
+      failureOrSuccess: failureOrSuccess,
+    );
+  }
+}
+
+/// @nodoc
+const $GetOtherFollowingWatcherState = _$GetOtherFollowingWatcherStateTearOff();
+
+/// @nodoc
+mixin _$GetOtherFollowingWatcherState {
+  bool get isLoading => throw _privateConstructorUsedError;
+  int? get id => throw _privateConstructorUsedError;
+  bool get showErrors => throw _privateConstructorUsedError;
+  List<FollowEntity>? get followList => throw _privateConstructorUsedError;
+  Either<Failure, List<FollowEntity>>? get failureOrSuccess =>
+      throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $GetOtherFollowingWatcherStateCopyWith<GetOtherFollowingWatcherState>
+      get copyWith => throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $GetOtherFollowingWatcherStateCopyWith<$Res> {
+  factory $GetOtherFollowingWatcherStateCopyWith(
+          GetOtherFollowingWatcherState value,
+          $Res Function(GetOtherFollowingWatcherState) then) =
+      _$GetOtherFollowingWatcherStateCopyWithImpl<$Res>;
+  $Res call(
+      {bool isLoading,
+      int? id,
+      bool showErrors,
+      List<FollowEntity>? followList,
+      Either<Failure, List<FollowEntity>>? failureOrSuccess});
+}
+
+/// @nodoc
+class _$GetOtherFollowingWatcherStateCopyWithImpl<$Res>
+    implements $GetOtherFollowingWatcherStateCopyWith<$Res> {
+  _$GetOtherFollowingWatcherStateCopyWithImpl(this._value, this._then);
+
+  final GetOtherFollowingWatcherState _value;
+  // ignore: unused_field
+  final $Res Function(GetOtherFollowingWatcherState) _then;
+
+  @override
+  $Res call({
+    Object? isLoading = freezed,
+    Object? id = freezed,
+    Object? showErrors = freezed,
+    Object? followList = freezed,
+    Object? failureOrSuccess = freezed,
+  }) {
+    return _then(_value.copyWith(
+      isLoading: isLoading == freezed
+          ? _value.isLoading
+          : isLoading // ignore: cast_nullable_to_non_nullable
+              as bool,
+      id: id == freezed
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as int?,
+      showErrors: showErrors == freezed
+          ? _value.showErrors
+          : showErrors // ignore: cast_nullable_to_non_nullable
+              as bool,
+      followList: followList == freezed
+          ? _value.followList
+          : followList // ignore: cast_nullable_to_non_nullable
+              as List<FollowEntity>?,
+      failureOrSuccess: failureOrSuccess == freezed
+          ? _value.failureOrSuccess
+          : failureOrSuccess // ignore: cast_nullable_to_non_nullable
+              as Either<Failure, List<FollowEntity>>?,
+    ));
+  }
+}
+
+/// @nodoc
+abstract class _$GetOtherFollowingWatcherStateCopyWith<$Res>
+    implements $GetOtherFollowingWatcherStateCopyWith<$Res> {
+  factory _$GetOtherFollowingWatcherStateCopyWith(
+          _GetOtherFollowingWatcherState value,
+          $Res Function(_GetOtherFollowingWatcherState) then) =
+      __$GetOtherFollowingWatcherStateCopyWithImpl<$Res>;
+  @override
+  $Res call(
+      {bool isLoading,
+      int? id,
+      bool showErrors,
+      List<FollowEntity>? followList,
+      Either<Failure, List<FollowEntity>>? failureOrSuccess});
+}
+
+/// @nodoc
+class __$GetOtherFollowingWatcherStateCopyWithImpl<$Res>
+    extends _$GetOtherFollowingWatcherStateCopyWithImpl<$Res>
+    implements _$GetOtherFollowingWatcherStateCopyWith<$Res> {
+  __$GetOtherFollowingWatcherStateCopyWithImpl(
+      _GetOtherFollowingWatcherState _value,
+      $Res Function(_GetOtherFollowingWatcherState) _then)
+      : super(_value, (v) => _then(v as _GetOtherFollowingWatcherState));
+
+  @override
+  _GetOtherFollowingWatcherState get _value =>
+      super._value as _GetOtherFollowingWatcherState;
+
+  @override
+  $Res call({
+    Object? isLoading = freezed,
+    Object? id = freezed,
+    Object? showErrors = freezed,
+    Object? followList = freezed,
+    Object? failureOrSuccess = freezed,
+  }) {
+    return _then(_GetOtherFollowingWatcherState(
+      isLoading: isLoading == freezed
+          ? _value.isLoading
+          : isLoading // ignore: cast_nullable_to_non_nullable
+              as bool,
+      id: id == freezed
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as int?,
+      showErrors: showErrors == freezed
+          ? _value.showErrors
+          : showErrors // ignore: cast_nullable_to_non_nullable
+              as bool,
+      followList: followList == freezed
+          ? _value.followList
+          : followList // ignore: cast_nullable_to_non_nullable
+              as List<FollowEntity>?,
+      failureOrSuccess: failureOrSuccess == freezed
+          ? _value.failureOrSuccess
+          : failureOrSuccess // ignore: cast_nullable_to_non_nullable
+              as Either<Failure, List<FollowEntity>>?,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$_GetOtherFollowingWatcherState
+    implements _GetOtherFollowingWatcherState {
+  const _$_GetOtherFollowingWatcherState(
+      {required this.isLoading,
+      this.id,
+      required this.showErrors,
+      this.followList,
+      this.failureOrSuccess});
+
+  @override
+  final bool isLoading;
+  @override
+  final int? id;
+  @override
+  final bool showErrors;
+  @override
+  final List<FollowEntity>? followList;
+  @override
+  final Either<Failure, List<FollowEntity>>? failureOrSuccess;
+
+  @override
+  String toString() {
+    return 'GetOtherFollowingWatcherState(isLoading: $isLoading, id: $id, showErrors: $showErrors, followList: $followList, failureOrSuccess: $failureOrSuccess)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _GetOtherFollowingWatcherState &&
+            const DeepCollectionEquality().equals(other.isLoading, isLoading) &&
+            const DeepCollectionEquality().equals(other.id, id) &&
+            const DeepCollectionEquality()
+                .equals(other.showErrors, showErrors) &&
+            const DeepCollectionEquality()
+                .equals(other.followList, followList) &&
+            const DeepCollectionEquality()
+                .equals(other.failureOrSuccess, failureOrSuccess));
+  }
+
+  @override
+  int get hashCode => Object.hash(
+      runtimeType,
+      const DeepCollectionEquality().hash(isLoading),
+      const DeepCollectionEquality().hash(id),
+      const DeepCollectionEquality().hash(showErrors),
+      const DeepCollectionEquality().hash(followList),
+      const DeepCollectionEquality().hash(failureOrSuccess));
+
+  @JsonKey(ignore: true)
+  @override
+  _$GetOtherFollowingWatcherStateCopyWith<_GetOtherFollowingWatcherState>
+      get copyWith => __$GetOtherFollowingWatcherStateCopyWithImpl<
+          _GetOtherFollowingWatcherState>(this, _$identity);
+}
+
+abstract class _GetOtherFollowingWatcherState
+    implements GetOtherFollowingWatcherState {
+  const factory _GetOtherFollowingWatcherState(
+          {required bool isLoading,
+          int? id,
+          required bool showErrors,
+          List<FollowEntity>? followList,
+          Either<Failure, List<FollowEntity>>? failureOrSuccess}) =
+      _$_GetOtherFollowingWatcherState;
+
+  @override
+  bool get isLoading;
+  @override
+  int? get id;
+  @override
+  bool get showErrors;
+  @override
+  List<FollowEntity>? get followList;
+  @override
+  Either<Failure, List<FollowEntity>>? get failureOrSuccess;
+  @override
+  @JsonKey(ignore: true)
+  _$GetOtherFollowingWatcherStateCopyWith<_GetOtherFollowingWatcherState>
+      get copyWith => throw _privateConstructorUsedError;
+}
--- hastaakalaa_app/lib/features/follow/presentation/bloc/get_other_following_watcher_bloc/get_other_following_watcher_state.dart
+++ hastaakalaa_app/lib/features/follow/presentation/bloc/get_other_following_watcher_bloc/get_other_following_watcher_state.dart
@@ -1,6 +1,20 @@
 part of 'get_other_following_watcher_bloc.dart';
 
-@immutable
-abstract class GetOtherFollowingWatcherState {}
+@freezed
+class GetOtherFollowingWatcherState with _$GetOtherFollowingWatcherState {
+  const factory GetOtherFollowingWatcherState({
+    required bool isLoading,
+    int? id,
+    required bool showErrors,
+    List<FollowEntity>? followList,
+    Either<Failure, List<FollowEntity>>? failureOrSuccess,
+  }) = _GetOtherFollowingWatcherState;
 
-class GetOtherFollowingWatcherInitial extends GetOtherFollowingWatcherState {}
+  factory GetOtherFollowingWatcherState.initial() =>
+      GetOtherFollowingWatcherState(
+        showErrors: false,
+        isLoading: false,
+        failureOrSuccess: null,
+        followList: [],
+      );
+}
