c39e1c59c2acaaeed6eb23a07a795081cd7e1aa0
BisheshDangol <bishesh01dangol@gmail.com>
2022-04-16 20:03:01 +0545

    Creating retrieve and change id events

--- hastaakalaa_app/lib/features/follow/presentation/bloc/get_other_following_watcher_bloc/get_other_following_watcher_bloc.dart
+++ hastaakalaa_app/lib/features/follow/presentation/bloc/get_other_following_watcher_bloc/get_other_following_watcher_bloc.dart
@@ -3,6 +3,8 @@ import 'package:dartz/dartz.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:hastaakalaa_app/core/errors/failures.dart';
 import 'package:hastaakalaa_app/features/follow/domain/entities/follow_entity.dart';
+import 'package:hastaakalaa_app/features/follow/domain/usecases/get_all_following_list_usecase.dart';
+import 'package:hastaakalaa_app/features/follow/domain/usecases/get_other_following_list_usecase.dart';
 import 'package:meta/meta.dart';
 
 part 'get_other_following_watcher_event.dart';
@@ -11,10 +13,26 @@ part 'get_other_following_watcher_bloc.freezed.dart';
 
 class GetOtherFollowingWatcherBloc
     extends Bloc<GetOtherFollowingWatcherEvent, GetOtherFollowingWatcherState> {
-  GetOtherFollowingWatcherBloc()
+  final GetOtherFollowingListUsecase _getOtherFollowingListUsecase;
+  GetOtherFollowingWatcherBloc(this._getOtherFollowingListUsecase)
       : super(GetOtherFollowingWatcherState.initial()) {
-    on<GetOtherFollowingWatcherEvent>((event, emit) {
-      // TODO: implement event handler
+    on<GetOtherFollowingWatcherEvent>((event, emit) async {
+      await event.map(changedId: (_ChangedId value) {
+        emit(
+          state.copyWith(
+            id: value.id,
+          ),
+        );
+      }, retrieveFollowList: (_RetrieveFollowList value) async {
+        emit(state.copyWith(isLoading: true, failureOrSuccess: null));
+
+        final followList = await _getOtherFollowingListUsecase.call(value.id);
+
+        emit(state.copyWith(
+            failureOrSuccess: followList,
+            isLoading: false,
+            followList: followList.fold((l) => [], (r) => r)));
+      });
     });
   }
 }
