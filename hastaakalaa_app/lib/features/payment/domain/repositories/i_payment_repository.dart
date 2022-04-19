04bc0f015eda75e1aa72c71f6241b2e0e96684c3
BisheshDangol <bishesh01dangol@gmail.com>
2022-04-19 07:39:23 +0545

    Creating get payment usecase

--- hastaakalaa_app/lib/features/payment/domain/repositories/i_payment_repository.dart
+++ hastaakalaa_app/lib/features/payment/domain/repositories/i_payment_repository.dart
@@ -1,7 +1,9 @@
 import 'package:dartz/dartz.dart';
 import 'package:hastaakalaa_app/core/errors/failures.dart';
+import 'package:hastaakalaa_app/features/payment/domain/entites/payment_entity.dart';
 
 abstract class IPaymentRepository {
   Future<Either<Failure, Unit>> createPayment(
       {required Map<String, dynamic> data});
+  Future<Either<Failure, List<PaymentEntity>>> getPayment();
 }
