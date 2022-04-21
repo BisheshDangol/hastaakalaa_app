930ee8b55bf540e62d8c415a60c9dd550d0c9ddb
BisheshDangol <bishesh01dangol@gmail.com>
2022-04-21 20:51:30 +0545

    Creating upload image bloc

---
+++ hastaakalaa_app/lib/features/user/presentation/bloc/bloc/upload_picture_form_event.dart
@@ -0,0 +1,4 @@
+part of 'upload_picture_form_bloc.dart';
+
+@immutable
+abstract class UploadPictureFormEvent {}
---
+++ hastaakalaa_app/lib/features/user/presentation/bloc/bloc/upload_picture_form_state.dart
@@ -0,0 +1,6 @@
+part of 'upload_picture_form_bloc.dart';
+
+@immutable
+abstract class UploadPictureFormState {}
+
+class UploadPictureFormInitial extends UploadPictureFormState {}
